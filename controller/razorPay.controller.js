27184e8e1709e65a2d312923a9bfafa0a37b38c2
raunak gurud <raunakgurud2121@gmail.com>
2023-01-21 14:56:51 +0530

    feat: payout with test keys and limit amount

--- controller/razorPay.controller.js
+++ controller/razorPay.controller.js
@@ -1,28 +1,25 @@
 const Razorpay = require("razorpay");
 const { v4: uuidv4 } = require("uuid");
 
-
 //create the razorpay instance
 let razorPayInstance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-
 //create the order with razorpay integration
 const createOrderRazorPay = async (req, res) => {
   console.log("amount");
-  const {amount}  = req.body;
-  
-  if(!amount) {
+  const { amount } = req.body;
+
+  if (!amount) {
     res.json({
-    message:"Please give the amount to pay"
-  })
-}
-  
+      message: "Please give the amount to pay",
+    });
+  }
 
   const uid = uuidv4();
-  console.log("uid->>", uid);
+  // console.log("uid->>", uid);
 
   orderRazorpayParams = {
     amount: amount * 100,
@@ -48,46 +45,38 @@ const createOrderRazorPay = async (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-
-  
 };
-
 
 //verify the razorpay id to successfully payment
 
-const verifyRazorPayOrder = async(req,res) => {
-     const {razorpay_order_id , razorpay_payment_id , razorpay_signature} = req.body;
+const verifyRazorPayOrder = async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
 
-    if(!razorpay_order_id || !razorpay_payment_id || !razorpay_signature){
-        res.json({
-            message:"Please give the all details to verify the order"
-          })
-    }
+  if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature) {
+    res.json({
+      message: "Please give the all details to verify the order",
+    });
+  }
 
-     
-    body = razorpay_order_id + "|" + razorpay_payment_id;
-    let crypto = require("crypto");
-    let expectedSignature = crypto
-      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
-      .update(body.toString())
-      .digest("hex");
+  body = razorpay_order_id + "|" + razorpay_payment_id;
+  let crypto = require("crypto");
+  let expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(body.toString())
+    .digest("hex");
 
+  if (expectedSignature === razorpay_signature) {
+    res.json({
+      success: true,
+      message: "Payment Successfully",
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: "Invalid Credentials",
+    });
+  }
+};
 
-      if (expectedSignature === razorpay_signature) {
-        
-        res.json({
-          success: true,
-          message: "Payment Successfull",
-        });
-      } else {
-        return res.json({
-            success: false,
-            message: "Invalid Crediantals",
-          });
-      }
-
-}
-
-
-
-module.exports = {createOrderRazorPay,verifyRazorPayOrder}
+module.exports = { createOrderRazorPay, verifyRazorPayOrder };
